fccf72f82946b5c44aa6ee305934640a93deb7ca
Stutii-Chauhan <stutiichauhan@gmail.com>
2025-05-05 12:30:34 +0530

    Update upload_to_supabase.py

--- upload_to_supabase.py
+++ upload_to_supabase.py
@@ -8,19 +8,26 @@ password = os.environ["SUPABASE_PASSWORD"]
 host = os.environ["SUPABASE_HOST"]
 port = os.environ["SUPABASE_PORT"]
 
+
+
+
+print("🔍 DEBUGGING ENVIRONMENT VARIABLES")
+print("HOST:", os.environ.get("SUPABASE_HOST"))
+print("USER:", os.environ.get("SUPABASE_USER"))
+print("DB:", os.environ.get("SUPABASE_DB"))
 engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
 
-for file in os.listdir("."):
-    if file.endswith(".xlsx") or file.endswith(".csv"):
-        if file.endswith(".xlsx"):
-            df = pd.read_excel(file)
-        else:
-            df = pd.read_csv(file)
+# for file in os.listdir("."):
+#     if file.endswith(".xlsx") or file.endswith(".csv"):
+#         if file.endswith(".xlsx"):
+#             df = pd.read_excel(file)
+#         else:
+#             df = pd.read_csv(file)
 
-        df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
-        table_name = file.lower().replace(".xlsx", "").replace(".csv", "").replace(" ", "_")
-        df.to_sql(table_name, engine, if_exists="replace", index=False)
-        print(f" Uploaded: {table_name}")
+#         df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+#         table_name = file.lower().replace(".xlsx", "").replace(".csv", "").replace(" ", "_")
+#         df.to_sql(table_name, engine, if_exists="replace", index=False)
+#         print(f" Uploaded: {table_name}")
 
 #fixed some host name
 #still didn't work
