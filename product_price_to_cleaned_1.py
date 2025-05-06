e6b28db25eb9b04dd94316bbc0c445c13c20156a
Stutii-Chauhan <stutiichauhan@gmail.com>
2025-05-06 12:18:14 +0530

    Update product_price_to_cleaned_1.py

--- product_price_to_cleaned_1.py
+++ product_price_to_cleaned_1.py
@@ -256,5 +256,5 @@ df.loc[
 
 df = df.drop(columns=["model_number", "asin","brand_name"])
 
-df.to_sql("product_price_cleaned", con=engine, if_exists="replace", index=False)
+df.to_sql("product_price_cleaned_output", con=engine, if_exists="replace", index=False)
 print("✅ Cleaned product_price saved as product_price_cleaned")
